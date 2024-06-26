3e49ef2fc40eb35e8995ff4a6ca4d9497d8ae87b
antagonist13 <1703shakayaroslaw@gmail.com>
2024-06-26 22:31:51 +0300

    try to fix 404

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './style.css'
-import "./basic/1"
+import "../basic/1"
 import "./basic/2"
-import "./basic/3"
-import "./basic/4"
-import "./basic/5"
-import "./basic/6"
-import "./basic/7"
+import "../basic/3"
+import "../basic/4"
+import "../basic/5"
+import "../basic/6"
+import "../basic/7"
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
